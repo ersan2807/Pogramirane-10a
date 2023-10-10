26866c823b1566037d8829bc9737490277f7e344
ersan2807 <124554664+ersan2807@users.noreply.github.com>
2023-10-10 20:31:11 +0300

    Add files via upload

---
+++ Dom13.10.23/Dom13.10.23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Enter your birthdate (DD/MM/YYYY):
+        Console.Write("Enter your birthdate (DD/MM/YYYY): ");
+        DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+        DateTime today = DateTime.Now;
+
+        TimeSpan difference = today - birthDate;
+
+        ulong millisecondsPassed = (ulong)difference.TotalMilliseconds;
+
+        ulong daysPassed = millisecondsPassed / (ulong)(1000 * 60 * 60 * 24);
+
+        Console.WriteLine("The number of days passed from your birthdate to today is: " + daysPassed);
+    }
+}
+
+    }
+}
