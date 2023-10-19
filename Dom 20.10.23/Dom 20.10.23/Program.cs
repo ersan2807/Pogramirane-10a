e9b539659af6a58515693988c53f91d0781ab6bd
ersan2807 <124554664+ersan2807@users.noreply.github.com>
2023-10-19 21:15:09 +0300

    Add files via upload

---
+++ Dom 20.10.23/Dom 20.10.23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dom_20._10._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //S=V*t+a*t*t/2
+            sbyte V = 0;
+            Console.Write("Enter the time-t{0}");
+            sbyte t = sbyte.Parse(Console.ReadLine());
+            Console.Write("Enter a{0}");
+            uint a = uint.Parse(Console.ReadLine());
+            ulong S = (ulong)(V * t + a * t * t / 2);
+            Console.WriteLine("Ranoto uskoritalno dvijenie e = {f2:0}"+S);
+        }
+    }
+}
