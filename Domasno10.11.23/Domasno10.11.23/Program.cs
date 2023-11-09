4b87a47d60affe39d5d5e5d314f5463cd654717f
ersan2807 <124554664+ersan2807@users.noreply.github.com>
2023-11-09 18:52:13 +0200

    Add files via upload

---
+++ Domasno10.11.23/Domasno10.11.23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of rows (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of columns (m): ");
+        int m = int.Parse(Console.ReadLine());
+        int[,] array = new int[n, m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"Enter element [{i},{j}]: ");
+                array[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Even numbers in row {i}: ");
+            for (int j = 0; j < m; j++)
+            {
+                if (array[i, j] % 2 == 0)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
